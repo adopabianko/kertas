95223f6a92af1bc0af256a31e4c275bf4fd75609
Ado Pabianko <ado.pabianko@railink.co.id>
2020-05-17 23:14:31 +0700

    add readme file

---
+++ README.md
@@ -0,0 +1,18 @@
+# Kertas
+Sebuah perpustakaan untuk mencatat aktifitas API.
+
+# Fitur
+Dapat mencatat aktifitas API berdasarkan Folder Tahun > Folder Bulan > File Tanggal.
+
+![alt text](example.png  "Example")
+
+# Contoh Penggunaan
+Clone :
+```bash
+$ git clone https://github.com/adopabianko/kertas.git
+```
+
+Jalankan example.php di console:
+```bash
+$ php example.php
+```
--- example.php
+++ example.php
@@ -3,9 +3,9 @@
 require_once('Kertas.php');
 
 $kertas = new Kertas();
-$kertas->create(['GET', '119.110.87.74', 'https://www.example.com?param=value', '{"status": "00"}']);
-$kertas->create(['POST', '119.110.87.74', 'https://www.example.com/login', '{"email": "adopabianko@gmail.com", "password": "admin123"}, {"status": "00"}']);
-$kertas->create(['GET', '119.110.87.74', 'https://www.example.com?param=value', '{"status": "00"}']);
-$kertas->create(['POST', '119.110.87.74', 'https://www.example.com/login', '{"email": "adopabianko@gmail.com", "password": "admin123"}, {"status": "00"}']);
-$kertas->create(['GET', '119.110.87.74', 'https://www.example.com?param=value', '{"status": "00"}']);
-$kertas->create(['POST', '119.110.87.74', 'https://www.example.com/login', '{"email": "adopabianko@gmail.com", "password": "admin123"}, {"status": "00"}']);
+$kertas->create(['GET', '127.0.0.1', 'https://www.example.com?param=value', '{"status": "00"}']);
+$kertas->create(['POST', '127.0.0.1', 'https://www.example.com/login', '{"email": "adopabianko@gmail.com", "password": "admin123"}, {"status": "00"}']);
+$kertas->create(['GET', '127.0.0.1', 'https://www.example.com?param=value', '{"status": "00"}']);
+$kertas->create(['POST', '127.0.0.1', 'https://www.example.com/login', '{"email": "adopabianko@gmail.com", "password": "admin123"}, {"status": "00"}']);
+$kertas->create(['GET', '127.0.0.1', 'https://www.example.com?param=value', '{"status": "00"}']);
+$kertas->create(['POST', '127.0.0.1', 'https://www.example.com/login', '{"email": "adopabianko@gmail.com", "password": "admin123"}, {"status": "00"}']);
